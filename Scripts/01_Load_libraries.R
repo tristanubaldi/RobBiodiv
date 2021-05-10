b778d6b4170841d319d55f4c60620320a09ee665
Tristan Ubaldi <tristan.ubaldi@gmail.com>
2021-05-10 15:04:12 +0300

    exports

---
+++ Scripts/01_Load_libraries.R
@@ -0,0 +1,13 @@
+##########################################################################################
+#
+#                                        LOAD LIBRARIES
+#
+##########################################################################################
+# May 2021
+# Tristan Ubaldi
+# R, Rstudio need to be updated
+# run INSTALATION to install all packages before loading them
+
+# # INSTALATION
+
+#  LOAD
---
+++ Scripts/02_import_data.R
@@ -0,0 +1,12 @@
+#################################################################################
+#
+#                                IMPORT DATA 
+#                           robinia analysis project
+#
+#################################################################################
+# Clean R space
+graphics.off()
+rm(list=ls())
+
+# Load libraries
+source("Scripts/01_Load_libraries.r")
