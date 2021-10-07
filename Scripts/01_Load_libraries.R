d08258c2790618a7eb54ec7053913a2af92cc3bd
Tristan Ubaldi <tristan.ubaldi@gmail.com>
2021-10-07 18:08:42 +0200

    Update load libraries file

--- Scripts/01_Load_libraries.R
+++ Scripts/01_Load_libraries.R
@@ -3,11 +3,137 @@
 #                                        LOAD LIBRARIES
 #
 ##########################################################################################
-# May 2021
-# Tristan Ubaldi
+# Ubaldi Tristan
 # R, Rstudio need to be updated
 # run INSTALATION to install all packages before loading them
 
+  
+
 # # INSTALATION
+# 
+# # General
+# install.packages("devtools")
+# install.packages("knitr")
+# 
+# 
+# # Community
+# install.packages("vegan")
+# install.packages("ade4")
+# install.packages("FD")
+# install.packages("RVAideMemoire")
+# install.packages("indicspecies")
+# install.packages("mclust")
+# install.packages("labdsv")
+# install.packages("betapart")
+# install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis", force = TRUE) 
+# 
+# 
+# # Data managment
+# install.packages("reshape")
+# install.packages("reshape2")
+# install.packages("plyr")
+# install.packages("dplyr")
+# install.packages("tidyr")
+# install.packages("gridExtra")
+# install.packages("stringr")
+# install.packages("stringi")
+# install.packages("data.table")
+# install.packages("tiblle")
+# devtools::install_github("jinyizju/S.PhyloMaker")
+# install.packages("tidyverse") 
+# install.packages("openxlsx")
+#
+# # Analysis
+# install.packages("lme4")
+# install.packages("effects")
+# install.packages("AICcmodavg")
+# install.packages("MuMIn")
+# install.packages("car")
+# install.packages("arm")
+# install.packages("effects")
+# install.packages("bbmle")
+# install.packages("sjPlot")
+# install.packages("multcomp")
+# install.packages("lmerTest")
+# install.packages("simr")
+# install.packages("picante")
+# install.packages("taxize")
+# install.packages("rstatix")
+# library(devtools)
+# install_github("sdray/adespatial")
+
+# # Plot
+# install.packages("ggplot2")
+# install.packages("ggrepel")
+# install.packages("ggpubr")
+# install.packages("rworldmap")
+# install.packages("mapdata")
+# install.packages('cowplot')
 
 #  LOAD
+
+# General
+library(devtools)
+library(knitr)
+
+# Community
+library(vegan)
+library(ade4)
+library(FD)
+library(RVAideMemoire)
+library(indicspecies)
+library(mclust)
+library(labdsv)
+library(betapart)
+library(pairwiseAdonis)
+
+
+# Data managment
+library(reshape)
+library(reshape2)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(gridExtra)
+library(stringr)
+library(stringi)
+library(data.table)
+library(V.PhyloMaker)
+library(ape)
+library(tidyverse)
+library(openxlsx)
+
+# Analysis
+library(lme4)
+library(effects)
+library(AICcmodavg)
+library(MuMIn)
+library(car)
+library(arm)
+library(stats)
+library(bbmle)
+library(sjPlot)
+library(multcomp)
+library(lmerTest)
+library(simr)
+library(picante)
+library(taxize)
+library(rstatix)
+library("adespatial")
+
+# Plot
+library(ggplot2)
+library(ggrepel)
+library(ggpubr)
+library(rworldmap)
+library(mapdata)
+library(ggtree)
+library(cowplot)
+
+
+
+
+
+
+
+
--- Scripts/02_import_data.R
+++ Scripts/02_import_data.R
@@ -9,10 +9,10 @@ graphics.off()
 rm(list=ls())
 
 # Load libraries
-source("01_Load_libraries.r")
+source("Scripts/01_Load_libraries.r")
+#
 
 ###### Database ###### 
 # Load Data Base
 
-robdata <- read.csv("Data/Rob_data.csv", stringsAsFactors=FALSE, header=TRUE,  
-                 sep=";", dec=",")
+robdata <- read.xlsx("Data/Synthese_data.xlsx")
